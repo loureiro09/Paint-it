6807b0893ed1415eeabb4b5ccf10b1201d96321d
loureiro09 <ana.dantas69@aluno.ce.gov.br>
2024-06-12 10:37:02 -0300

    first commit

---
+++ tintas.js
@@ -0,0 +1,30 @@
+function bt(){
+    document.querySelector("body").style.backgroundColor = "red"
+}
+function bo(){
+    document.querySelector("body").style.backgroundColor = "orange"
+}
+function ba(){
+    document.querySelector("body").style.backgroundColor = "yellow"
+}
+function bot(){
+    document.querySelector("body").style.backgroundColor = "green"
+}
+function ao(){
+    document.querySelector("body").style.backgroundColor = "blue"
+}
+function bat(){
+    document.querySelector("body").style.backgroundColor = "darkblue"
+}
+function bato(){
+    document.querySelector("body").style.backgroundColor = "purple"
+}
+function a(){
+    alert("Somos Paint it, uma loja de tintas de qualidade!")
+}
+function b(){
+    alert("Use os botões à direita para testar cores de tinta")
+}
+function c(){
+    alert("Número: (88) 99940-6462")
+}
